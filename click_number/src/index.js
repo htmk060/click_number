916b51ae8d9ce339fd220e353ebd90c67b4ba8c4
DESKTOP-MT5F8FL\a060ffu <tomoaki20200313@yahoo.co.jp>
2023-06-18 14:27:29 +0900

    最初のコミット

---
+++ click_number/src/index.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+for (var num = 9; num > 0; num--) {
+  var elm = document.createElement("button");
+  elm.innerHTML = num;
+
+  elm.setAttribute("id", num);
+  elm.setAttribute("class", "circle");
+  var function_name = "remove(" + num + ")";
+
+  let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  elm.style.background = color;
+
+  var num2 = Math.random() * 50 + 50;
+  elm.style.height = num2 + "px";
+  elm.style.width = num2 + "px";
+
+  elm.setAttribute("onclick", function_name);
+  document.getElementById("main").appendChild(elm);
+
+  var left_pos = 10;
+  var top_pos = 100;
+
+  left_pos = left_pos + Math.floor(Math.random() * 400);
+  top_pos = top_pos + Math.floor(Math.random() * 600);
+
+  document.getElementById(num).style.left = "" + left_pos + "px";
+  document.getElementById(num).style.top = "" + top_pos + "px";
+}
+
+var next = 1;
+document.remove = function (id) {
+  if (id === next) {
+    document.getElementById("main").removeChild(document.getElementById(id));
+    next = next + 1;
+  }
+};
